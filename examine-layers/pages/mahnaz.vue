affc352fc50d0ab8da0b8e9304d5491a2b3654a5
mahnaz_afshar <mahnazi.afsharpour@gmail.com>
2023-09-20 08:56:31 +0330

    veri code

--- examine-layers/pages/mahnaz.vue
+++ examine-layers/pages/mahnaz.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="p-5 flex flex-col">
-  text input:
-    <v-text-input v-model="name" :select="false" :focus="true"       type="number"
-      inputmode="numeric"></v-text-input>
-    verification code input :{{ code }}
- 
-    <v-verification-code-input v-model="code" :length="4" data-input-delete="h-12 w-12" data-input-add="h-14 w-14"></v-verification-code-input>
+    text input:
+    <v-form @submit="onSubmit" :validation-schema="schema">
+      verification code input :{{ code }}
 
-    <!-- <test-pagination></test-pagination>
+      <v-verification-code-input
+        v-model="code"
+        name="code"
+        :length="4"
+        data-input-delete="h-12 w-12"
+        data-input-add="h-14 w-14"
+      ></v-verification-code-input>
+      <d-button class="btn btn-primary" type="submit">submit </d-button>
+    </v-form>
+    <test-pagination></test-pagination>
     test Daisy-Forms
     <hr />
     <TestForm></TestForm>
@@ -18,11 +24,21 @@
     <hr />
     <div class="p-3 flex flex-col gap-4">
       <TestDAccordion></TestDAccordion>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import * as yup from "yup";
 const name = ref("");
 const code = ref("");
+// const birthday = ref("1368/3/2");
+const birthday = ref("");
+const schema = yup.object({
+  // birthday: yup.string().required(),
+  code: yup.string().required(),
+});
+function onSubmit(values: any) {
+  console.log("click submit", values);
+}
 </script>
