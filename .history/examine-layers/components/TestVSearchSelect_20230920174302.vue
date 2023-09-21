fe1e19744587991775b73c87c7abddde20da8606
mahnaz_afshar <mahnazi.afsharpour@gmail.com>
2023-09-21 08:39:12 +0330

    create search select

--- .history/examine-layers/components/TestVSearchSelect_20230920174302.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <v-search-select :data="data"></v-search-select>
-</template>
-<script lang="ts" setup>
-const data = ["John Smith", "John Doe", "Jane Doe", "Phillip Green", "Peter Brown"];
-</script>
