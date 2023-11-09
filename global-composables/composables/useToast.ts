c35f008edb765e5603f637712f012239b60a84de
ziaadini <y.ziaadini@gmail.com>
2023-11-09 11:54:06 +0330

    toast

--- global-composables/composables/useToast.ts
+++ global-composables/composables/useToast.ts
@@ -1,17 +1,25 @@
-import { useDebounceFn } from '@vueuse/core'
+import { ToastEnum } from '../utils/TToast.enum';
 interface Toast {
     message: string;
     type: ToastEnum;
     show: boolean;
+    [key:string|number]:any
 }
-export const useToast = () => {
-    const toastRef = useState<Toast>('--toast--', () => ({ show: false, message: '', type: ToastEnum.info }));
-    const debounceClose = useDebounceFn(() => {
-        toastRef.value = { ...toastRef.value, show: false }
-    }, 3000)
-    const showToast = ({ message, type = ToastEnum.info }: { message: string, type:  ToastEnum }) => {
-        toastRef.value = { message, type, show: true };
-        debounceClose();
-    }
-    return { showToast, toastRef };
+export const useToast = (key='--toast--') => {
+  const toastRef = useState<Toast>(key, () => ({ show: false, message: '', type: ToastEnum.info }));
+  const close=() => {
+      toastRef.value = { ...toastRef.value, show: false }
+  }
+
+  let timeout:any;
+  const showToast = ({ message, type = ToastEnum.info,duration,...extraConfig }: { message: string, type:  ToastEnum,duration?:number,[key:string|number]:any }) => {
+      toastRef.value = { message, type, ...extraConfig ,show: true };
+      if(timeout!=undefined){
+          clearTimeout(timeout)
+      }
+       timeout=setTimeout(()=>{
+          close();
+      },duration)
+  }
+  return { showToast, toastRef };
 }
