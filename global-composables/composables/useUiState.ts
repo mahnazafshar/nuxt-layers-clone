7b071e06f3bfc9dc4cf24bc4f70af12a15d3d08f
ziaadini <y.ziaadini@gmail.com>
2024-03-30 11:01:43 +0330

    use ui state

--- global-composables/composables/useUiState.ts
+++ global-composables/composables/useUiState.ts
@@ -1,41 +1,52 @@
 import { Ref } from "vue"
-export const useUiState = (sharedKey = '',config:{closeOnUnmounted?:boolean}={}) => {
-    const defineRef = <K>(defaultValue: K, prefix = '') => {
-        if (sharedKey) {
-            return useState<K>(prefix + sharedKey, () => defaultValue) as Ref<K>
-        }
-        return ref<K>(defaultValue)
+export const useUiState = (sharedKey = '',config:{closeOnUnmounted?:boolean,handleBackButton?:boolean}={}) => {
+  const defineRef = <K>(defaultValue: K, prefix = '') => {
+    if (sharedKey) {
+      return useState<K>(prefix + sharedKey, () => defaultValue) as Ref<K>
     }
-    const model = defineRef<boolean>(false)
-    const resetKey = defineRef<number>(0,'-component-key-')
-    const open = () => {
-        model.value = true
+    return ref<K>(defaultValue)
+  }
+  const model = defineRef<boolean>(false)
+  const resetKey = defineRef<number>(0,'-component-key-')
+  const open = () => {
+    model.value = true
+    if(config.handleBackButton){
+    window.history.pushState('backPressed', null, null);
+    window.history.pushState('dummy', null, null);
+    window.addEventListener('popstate', hide, { once: true });
     }
-    const close = () => {
-        model.value = false
+  }
+
+  const hide = function(event:any) {
+    if (event.state == 'backPressed') {
+     close();
     }
-    const toggle = () => {
-        if (model.value) {
-            close()
-        } else {
-            open()
-        }
+  };
+  const close = () => {
+    model.value = false
+  }
+  const toggle = () => {
+    if (model.value) {
+      close()
+    } else {
+      open()
     }
-    const setState = (state: boolean) => {
-        model.value = state
-    }
-    const openWithReset=()=>{
-      resetKey.value++;
-      open();
-    }
-    const closeWithReset=()=>{
-      resetKey.value++
+  }
+  const setState = (state: boolean) => {
+    model.value = state
+  }
+  const openWithReset=()=>{
+    resetKey.value++;
+    open();
+  }
+  const closeWithReset=()=>{
+    resetKey.value++
+    close();
+  }
+  onUnmounted(() => {
+    if(config.closeOnUnmounted){
       close();
     }
-    onUnmounted(() => {
-      if(config.closeOnUnmounted){
-        close();
-      }
-    })
-    return { model, open, close, toggle,openWithReset,closeWithReset, setState,resetKey }
+  })
+  return { model, open, close, toggle,openWithReset,closeWithReset, setState,resetKey }
 }
